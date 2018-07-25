45d1af0046dced7057405485c17bda1da0b7dbd9
Soldat72 <41701290+Soldat72@users.noreply.github.com>
2018-07-25 22:14:29 +0300

    Update game1.py

--- lesson2/game1.py
+++ lesson2/game1.py
@@ -2,21 +2,18 @@ import random
 def main():
     number = int(random.randint(1,7))
     print(number)
-    #running = True
-    while True: #running:
+     while True: 
         guess = int(input("введите число от 1 до 7:"))
         if guess == number :
             print('That right')
             print('Ok')
-            #running = False
             return True
         elif guess < number :
             print('too small')
         else :
             print('too many')
-
     else :
-        print('цикл закончен')
+        print('end of cycle')
     print('that is all')
 
 main()
