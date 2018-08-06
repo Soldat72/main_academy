d69deb815ba6ee83414cdac442ba9378e3aab75f
Soldat72 <41701290+Soldat72@users.noreply.github.com>
2018-08-06 17:34:28 +0300

    Add files via upload

---
+++ lesson2/lab2_1_1.py
@@ -0,0 +1,9 @@
+var1 = 5
+print('Тип переменной var1:', type(var1))
+print(' var1 есть Тrue or False:', var1 is True)
+var2 = var1 or True
+print(' var2 есть Тrue or False:', var2 is True)
+print(var1 and var2)
+print(var1 or var2)
+var2 = 6
+print(var1 != var2)
