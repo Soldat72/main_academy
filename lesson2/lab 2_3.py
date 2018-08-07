7e8edce3d4010470e3788518d694dbe5d3264984
Soldat72 <41701290+Soldat72@users.noreply.github.com>
2018-08-07 10:21:15 +0300

    Update lab 2_3.py

--- lesson2/lab 2_3.py
+++ lesson2/lab 2_3.py
@@ -1,11 +1,16 @@
 import string
 import random
-a=string.ascii_letters
-#b= random.sample(a,5)
+a=string.ascii_letters+string.digits
+b= random.sample(a,9)
 print('первая буква:', a[0], 'последняяя буква:',a[-1])
-print('первая третья:', a[2], 'последняяя третья:',a[-2])
+print('первая третья:', a[2],'\n' 'последняяя третья:',a[-2])
 print('первые 8:', a[0:8])
 print('первые 9,10:', a[9:11])
 print('8-10:', a[0:8]+a[9:11])
 l=int(len(a)/2)
-print('середина', a[l])
+print (l)
+print('середина списка:', a[l])
+print(b)
+print('Каждая третья из спискаэ:', b[::3])
+print('Весь список', a[:])
+print('Реверс списка', a[::-1])
